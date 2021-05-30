a7a01e698585cfcae62c5d27c9878f2356736385
Hasan <hasanuzzaman.dev@gmail.com>
2021-05-30 13:03:09 +0600

    raised button and onPresed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 choosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text('The Question! '),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Answer 2 Choosen"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                print('Answer 3 Choosen!');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
-  
 }
